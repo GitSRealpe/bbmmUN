05bc562cbd2bf3eebe1f5fa800437def5881a795
GitSRealpe <msn-realpe@hotmail.com>
2022-02-27 17:06:15 -0500

    display current breakpoint size on screen

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 function resized() {
     target = document.getElementById("medidas");
-    target.innerHTML = window.innerWidth;
+    ancho = window.innerWidth
+    if (ancho > 1400) {
+        target.innerHTML = window.innerWidth + " xxl ";
+    }
+    if (ancho < 1400 && ancho > 1200) {
+        target.innerHTML = window.innerWidth + " xl ";
+    }
+    if (ancho < 1200 && ancho > 992) {
+        target.innerHTML = window.innerWidth + " lg ";
+    }
+    if (ancho < 992 && ancho > 768) {
+        target.innerHTML = window.innerWidth + " md ";
+    }
+    if (ancho < 768 && ancho > 576) {
+        target.innerHTML = window.innerWidth + " sm ";
+    }
+    if (ancho < 576) {
+        target.innerHTML = window.innerWidth + " xs ";
+    }
+
 }
 
 window.addEventListener('resize', resized);
