6914c77175aff57393422287af0e15bb1136360d
GitSRealpe <msn-realpe@hotmail.com>
2022-03-09 20:43:55 -0500

    background and sizing edits

--- js/index.js
+++ js/index.js
@@ -25,29 +25,23 @@ window.onload = function() {
     window.addEventListener('resize', resized);
     resized();
 
-    // // init controller
+    // init controller
     var controller = new ScrollMagic.Controller();
 
-    // // build scene
-    new ScrollMagic.Scene({
-            triggerElement: "#trigger1",
-            triggerHook: 0.6, // show, when scrolled 10% into view
-            // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
-            offset: 70 // move trigger to center of element
-        })
-        .setClassToggle("#reveal1", "visible") // add class to reveal
-        .addIndicators() // add indicators (requires plugin)
-        .addTo(controller);
+    let elems = [".obeso", ".obesidad"];
+    // build scene
+    for (let i = 0; i < elems.length; i++) {
+        new ScrollMagic.Scene({
+                triggerElement: "#trigger1",
+                triggerHook: 0.7, // show, when scrolled 10% into view
+                // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+                offset: 70 // move trigger to center of element
+            })
+            .setClassToggle(elems[i], "visible") // add class to reveal
+            .addIndicators() // add indicators (requires plugin)
+            .addTo(controller);
+    }
 
-    // init
-    // var controller = new ScrollMagic.Controller({
-    //     globalSceneOptions: {
-    //         triggerHook: 'onLeave',
-    //         duration: "100%" // this works just fine with duration 0 as well
-    //             // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-    //             // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
-    //     }
-    // });
 
     // get all slides
     var slides = document.querySelectorAll("section.panel");
